9c6d1b77672bcd3c7294ddb66bd139e1fc9bd99d
bala-amypo <bala@amypo.in>
2025-12-18 16:43:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/FraudAlertRecord.java
+++ src/main/java/com/example/demo/entity/FraudAlertRecord.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 public class FraudAlertRecord{
     private Long id;
-    private String serialNumber;
-    
+    private String ruleCode;
+    private String claimantName;
+    private String claimantEmail;
+
 }
