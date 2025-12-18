e312165e13e72e1d75eccc396f708dbf1492beff
bala-amypo <bala@amypo.in>
2025-12-18 17:01:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/FraudAlertRecord.java
+++ src/main/java/com/example/demo/entity/FraudAlertRecord.java
@@ -4,5 +4,10 @@ import jakarta.persistence.*;
 public class FraudAlertRecord{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    
+    private Long id;
+    private Long claimId;
+    private String serialNumber;
+    private String alertType;
+    private String severity;
+    private String 
 }
