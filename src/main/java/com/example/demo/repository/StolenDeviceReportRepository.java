fdb25a6099fdc44fcf27efc9da11cbce697bed14
bala-amypo <bala@amypo.in>
2025-12-18 18:39:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
+++ src/main/java/com/example/demo/repository/StolenDeviceReportRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.spring
-public interface StolenDeviceReportRepository{
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.StolenDeviceReport;
+@Repository
+public interface StolenDeviceReportRepository extends JpaRepository<StolenDeviceReport,INTEGER>{
 
 }
