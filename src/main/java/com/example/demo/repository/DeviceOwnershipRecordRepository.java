1af746029c23bf64b7f2a29b5276ded7ae554a79
bala-amypo <bala@amypo.in>
2025-12-18 18:32:43 +0000

    User commit

--- src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
+++ src/main/java/com/example/demo/repository/DeviceOwnershipRecordRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.project.entity.DeviceOwnershipRecord;
+import com.example.demo.entity.DeviceOwnershipRecord;
 @Repository
 public interface DeviceOwnershipRecordRepository extends JpaRepository<DemoOwnershipRecord,INTEGER>{
     
--- src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
+++ src/main/java/com/example/demo/repository/FraudAlertRecordRepository.java
@@ -1,7 +1,8 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.
-public interface FraudAlertRecordRepository{
+import com.example.demo.entity.FraudAlertRecord;
+@Repository
+public interface FraudAlertRecordRepository extends FraudAlertRecord{
 
 }
