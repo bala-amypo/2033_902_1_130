efa4b3373a02f4cee1ae22664a6c0e9b970d784f
bala-amypo <bala@amypo.in>
2025-12-19 04:16:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
-public interface WarrantyClaimRecordRepository{
+import org.springframework.data.jap.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.
+@Repository
+public interface WarrantyClaimRecordRepository extends JpaRepository<WarrantyClaimRecord,INTEGER>{
 
 }
