40830edf95b3d0a800b763dde634920b07f3a5a1
bala-amypo <bala@amypo.in>
2025-12-20 13:47:07 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.implement;
 import com.example.demo.service.DeviceOwnershipService;
-public interface DeviceOwnershipServiceImplement{
+public class DeviceOwnershipServiceImplement implements DeviceOwnershipService{
 
 }
--- src/main/java/com/example/demo/service/implement/FraudAlertServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/FraudAlertServiceImplement.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.implement;
 import com.example.demo.service.FraudAlertService;
-public interface FraudAlertServiceImplement{
+public class FraudAlertServiceImplement implements FraudAlertService{
 
 }
