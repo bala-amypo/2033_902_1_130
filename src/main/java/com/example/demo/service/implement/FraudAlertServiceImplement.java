104d6f0d3c646fda83fcdd4175887b2e7ad71141
bala-amypo <bala@amypo.in>
2025-12-21 16:51:07 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/FraudAlertServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/FraudAlertServiceImplement.java
@@ -1,5 +1,37 @@
-package com.example.demo.service.implement;
-import com.example.demo.service.FraudAlertService;
-public class FraudAlertServiceImplement implements FraudAlertService{
+@Service
+public class FraudAlertServiceImplement implements FraudAlertService {
 
+    @Autowired
+    private FraudAlertRecordRepository repo;
+
+    // 1. Create a new fraud alert rule
+    @Override
+    public FraudAlertRecord createRule(FraudAlertRecord alert) {
+        alert.setResolved(false); // new alert is unresolved
+        return repo.save(alert);
+    }
+
+    @Override
+    public FraudAlertRecord resolveAlert(Long id) {
+        FraudAlertRecord alert = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Alert not found"));
+
+        alert.setResolved(true);  
+        return repo.save(alert);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getAlertBySerial(String serialNumber) {
+        return repo.findBySerialNumber(serialNumber);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getAlertByClaim(Long claimId) {
+        return repo.findByClaimId(claimId);
+    }
+
+    @Override
+    public List<FraudAlertRecord> getAllRules() {
+        return repo.findAll();
+    }
 }
