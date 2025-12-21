9f3814e44a504d3fa8ad169c1c38257964e5da34
bala-amypo <bala@amypo.in>
2025-12-21 15:00:07 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudAlertService.java
+++ src/main/java/com/example/demo/service/FraudAlertService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
-import java
+import java.util.List;
+import com.example.demo.model.FraudAlertRecord;
 public interface FraudAlertService{
     FraudAlertRecord createRule(FraudAlertRecord alert);
     FraudAlertRecord resolveAlert(Long id);
-    List<FraudAlertRecord>getAlertBySerial(String ser)
+    List<FraudAlertRecord>getAlertBySerial(String serialNumber);
+    List<FraudAlertRecord>getAlertByClaim(Long claimId);
+    List<FraudAlertRecord>getAllRules();
 }
