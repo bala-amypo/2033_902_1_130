e034bbf1186d36ace9a794754b7c01f9bade69d6
bala-amypo <bala@amypo.in>
2025-12-21 15:27:07 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.implement;
 import com.example.demo.service.DeviceOwnershipService;
+@Service
 public class DeviceOwnershipServiceImplement implements DeviceOwnershipService{
-
+    @Autowired DeviceOwnershipRecordRepository 
 }
