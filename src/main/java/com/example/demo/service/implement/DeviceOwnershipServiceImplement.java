bdfefa1bddbffeae5242a637de0e0d242efa810d
bala-amypo <bala@amypo.in>
2025-12-21 16:06:07 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
@@ -1,29 +1,41 @@
 package com.example.demo.service.implement;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.model.DeviceOwnershipRecord;
 import com.example.demo.repository.DeviceOwnershipRecordRepository;
 import com.example.demo.service.DeviceOwnershipService;
+
 @Service
-public class DeviceOwnershipServiceImplement implements DeviceOwnershipService{
-    @Autowired DeviceOwnershipRecordRepository repo;
+public class DeviceOwnershipServiceImplement implements DeviceOwnershipService {
+
+    @Autowired
+    private DeviceOwnershipRecordRepository repo;
+
     @Override
-        public DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord device){
-            return repo.save(device);
-        }
+    public DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord device) {
+        return repo.save(device);
+    }
+
     @Override
-        public List<DeviceOwnershipRecord>getBySerial(String serialNumber){
-            return repo.findBySerialNumber(serialNumber);
-        }
+    public List<DeviceOwnershipRecord> getBySerial(String serialNumber) {
+        return repo.findBySerialNumber(serialNumber);
+    }
+
     @Override
-        public List<DeviceOwnershipRecord>getAllDevices(){
-            return repo.findAll();
-        }
+    public List<DeviceOwnershipRecord> getAllDevices() {
+        return repo.findAll();
+    }
+
     @Override
-        public DeviceOwnershipRecord updateDeviceStatus(Long id,boolean active){
-            DeviceOwnershipRecord devices=repo.findById(id);
-            repo.setActive(active);
-            return device.save(devices);
-        }
+    public DeviceOwnershipRecord updateDeviceStatus(Long id, boolean active) {
+        DeviceOwnershipRecord device = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Device not found"));
+
+        device.setActive(active);
+        return repo.save(device);
+    }
 }
