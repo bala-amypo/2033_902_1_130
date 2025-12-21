c985fde03650cb4b4a5e3a2fddc3b9319440c73a
bala-amypo <bala@amypo.in>
2025-12-21 15:21:07 +0000

    User commit

--- src/main/java/com/example/demo/service/WarrantyClaimService.java
+++ src/main/java/com/example/demo/service/WarrantyClaimService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 import java.util.List;
-
+import com.example.demo.model.WarrantyClaimRecord;
 public interface WarrantyClaimService{
-
+    WarrantyClaimRecord submitClaim(WarrantyClaimRecord claim);
+    WarrantyClaimRecord 
 }
