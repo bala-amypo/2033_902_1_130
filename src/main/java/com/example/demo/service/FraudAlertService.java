8ba33a0a1b4a659d39d2f09765b784741a44b645
bala-amypo <bala@amypo.in>
2025-12-21 14:57:07 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudAlertService.java
+++ src/main/java/com/example/demo/service/FraudAlertService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
+import java
 public interface FraudAlertService{
     FraudAlertRecord createRule(FraudAlertRecord alert);
-    
+    FraudAlertRecord resolveAlert(Long id);
+    List<FraudAlertRecord>getAlertBySerial(String ser)
 }
