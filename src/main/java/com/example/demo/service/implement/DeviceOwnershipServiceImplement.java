8f02d51eafb096e42cfc67efd72bae07be5d635b
bala-amypo <bala@amypo.in>
2025-12-21 15:57:08 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
+++ src/main/java/com/example/demo/service/implement/DeviceOwnershipServiceImplement.java
@@ -1,5 +1,5 @@
 package com.example.demo.service.implement;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.DeviceOwnershipRecord;
@@ -7,23 +7,23 @@ import com.example.demo.repository.DeviceOwnershipRecordRepository;
 import com.example.demo.service.DeviceOwnershipService;
 @Service
 public class DeviceOwnershipServiceImplement implements DeviceOwnershipService{
-    @Autowired DeviceOwnershipRecordRepository device;
+    @Autowired DeviceOwnershipRecordRepository repo;
     @Override
         public DeviceOwnershipRecord registerDevice(DeviceOwnershipRecord device){
             return repo.save(device);
         }
     @Override
         public List<DeviceOwnershipRecord>getBySerial(String serialNumber){
-            return device.findBySerialNumber(serialNumber);
+            return repo.findBySerialNumber(serialNumber);
         }
     @Override
         public List<DeviceOwnershipRecord>getAllDevices(){
-            return device.findAll();
+            return repo.findAll();
         }
     @Override
         public DeviceOwnershipRecord updateDeviceStatus(Long id,boolean active){
-            DeviceOwnershipRecord devices=device.findById(id);
-            device.setActive(active);
+            DeviceOwnershipRecord devices=repo.findById(id);
+            repo.setActive(active);
             return device.save(devices);
         }
 }
