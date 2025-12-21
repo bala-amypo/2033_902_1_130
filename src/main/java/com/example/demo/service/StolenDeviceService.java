73ee2d17556d6b695f3533d1d5f728d0e852027f
bala-amypo <bala@amypo.in>
2025-12-21 15:09:07 +0000

    User commit

--- src/main/java/com/example/demo/service/StolenDeviceService.java
+++ src/main/java/com/example/demo/service/StolenDeviceService.java
@@ -3,4 +3,7 @@ import java.util.List;
 import com.example.demo.model.StolenDeviceReport;
 public interface StolenDeviceService{
     StolenDeviceReport reportStolen(StolenDeviceReport report);
+    List<StolenDeviceReport>getReportsBySerial(String serialNumber);
+    List<StolenDeviceReport>getReportById(Long id);
+    List<StolenDeviceReport>getAllReports();
 }
