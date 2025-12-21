06679d23a0234ec99e615211313fe8f3b35e525d
bala-amypo <bala@amypo.in>
2025-12-21 17:32:48 +0000

    User commit

--- src/main/java/com/example/demo/service/WarrantyClaimService.java
+++ src/main/java/com/example/demo/service/WarrantyClaimService.java
@@ -2,9 +2,9 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.WarrantyClaimRecord;
 public interface WarrantyClaimService{
-    //WarrantyClaimRecord submitClaim(WarrantyClaimRecord claim);
-    //WarrantyClaimRecord updateClaimStatus(Long claimId,String status);
-    //List<WarrantyClaimRecord>getClaimById(Long id);
-    //List<WarrantyClaimRecord>getClaimsBySerial(String serialNumber);
-    //List<WarrantyClaimRecord>getAllClaims();
+    WarrantyClaimRecord submitClaim(WarrantyClaimRecord claim);
+    WarrantyClaimRecord updateClaimStatus(Long claimId,String status);
+    List<WarrantyClaimRecord>getClaimById(Long id);
+    List<WarrantyClaimRecord>getClaimsBySerial(String serialNumber);
+    List<WarrantyClaimRecord>getAllClaims();
 }
